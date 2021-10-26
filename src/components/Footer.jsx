1d20fc6339890664bccf2bde28e61dbce317ffd6
PRACHIP09 <prachipp999@gmail.com>
2021-10-26 09:50:07 +0530

    Contact Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,8 @@ import React from "react";
 export const Footer = () => {
   return (
     <>
-<<<<<<< HEAD
       <div className="absolute bottom-100 w-full flex justify-center items-center h-16 bg-black text-white">
-=======
-      <div className="flex bottom-0 w-full fixed justify-center items-center h-16 bg-black text-white">
->>>>>>> 1f4594e38f5ca514606dbaf83dfd4e13141a8faa
+
         <p>Copyright © 2021 All rights reserved.</p>
       </div>
     </>
