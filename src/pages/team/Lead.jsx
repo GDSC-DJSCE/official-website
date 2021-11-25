a613916531cb2b6647a4890c422c4c87715d09b4
jinish08 <jinishshah08@gmailcom>
2021-11-25 10:44:46 +0530

    added all team pages

--- src/pages/team/Lead.jsx
+++ src/pages/team/Lead.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import "../../styles/team.css";
-import { TeamNav, THead, TCard } from "../../components";
+// import React from "react";
+// import "../../styles/team.css";
+// import { TeamNav, THead, TCard } from "../../components";
 
-export const Lead = () => {
-  return (
-    <>
-      <THead />
-      <TeamNav />
-      <div className="h-24 w-min mx-auto border-2-blue"></div>
-      <div className="flex justify-center">
-        <TCard />
-      </div>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-    </>
-  );
-};
+// export const Lead = () => {
+//   return (
+//     <>
+//       <THead />
+//       <TeamNav />
+//       <div className="h-24 w-min mx-auto border-2-blue"></div>
+//       <div className="flex justify-center">
+//         <TCard />
+//       </div>
+//       <br />
+//       <br />
+//       <br />
+//       <br />
+//       <br />
+//     </>
+//   );
+// };
--- src/pages/team/ProjectTeam.jsx
+++ src/pages/team/ProjectTeam.jsx
@@ -1,11 +1,120 @@
 import React from "react";
-import { TeamNav, THead } from "../../components";
+import "../../styles/team.css";
+import tree from "../../assets/images/team/Tree.svg";
+import back from "../../assets/images/team/Union.svg";
+import { Button, Typography } from "@mui/material";
+import { TCard } from "../../components";
+import { Link } from "react-router-dom";
 
-export const ProjectTeam = () => {
-  return (
-    <div>
-      <THead />
-      <TeamNav />
+export const ProjectTeam = () => (
+  <>
+    <p className="header">Our Team</p>
+    <div className="center">
+      <Button
+        style={{ borderRadius: "15px", width: "225px" }}
+        variant="contained"
+      >
+        <p
+          style={{
+            fontSize: "24px",
+            fontFamily: "Montserrat",
+            textTransform: "none",
+            fontWeight: "400",
+          }}
+        >
+          Project Team
+        </p>
+      </Button>
     </div>
-  );
-};
+    <div style={{ marginTop: "18px" }} className="center">
+      <TCard />
+    </div>
+    <img className="img" src={tree} alt="branches" />
+    <div className="cards">
+      <div className="cards-end">
+        <p>Frontend Team</p>
+        <TCard />
+      </div>
+      <div className="cards-center">
+        <p>Backend Team</p>
+        <TCard />
+      </div>
+      <div className="cards-center">
+        <p>Full Stack Developer</p>
+        <TCard />
+      </div>
+      <div className="cards-end">
+        <p>App Team</p>
+        <TCard />
+      </div>
+    </div>
+    <div className="cards">
+      <div className="cards-end">
+        <TCard />
+      </div>
+      <div className="cards-center">
+        <TCard />
+      </div>
+      <div className="cards-center">
+        <div style={{ width: "165px" }}></div>
+      </div>
+      <div className="cards-end">
+        <TCard />
+      </div>
+    </div>
+    <div className="cards">
+      <div className="cards-end">
+        <TCard />
+      </div>
+      <div className="cards-center">
+        <div style={{ width: "165px" }}></div>
+      </div>
+      <div className="cards-center">
+        <div style={{ width: "165px" }}></div>
+      </div>
+      <div className="cards-end">
+        <div style={{ width: "165px" }}></div>
+      </div>
+    </div>
+    <div className="bottom-div">
+      <div className="bottom-div-col">
+        <TCard />
+        <TCard />
+      </div>
+      <div className="bottom-div-col">
+        <TCard />
+        <TCard />
+      </div>
+    </div>
+    <div className="bottom-div">
+      <div className="bottom-div-col">
+        <TCard />
+        <TCard />
+      </div>
+      <div className="bottom-div-col">
+        <TCard />
+        <TCard />
+      </div>
+    </div>
+    <Link to="/team">
+      <div className="back">
+        <img src={back} alt="back" />
+        <Typography
+          style={{
+            fontFamily: " Montserrat",
+            fontSize: "24px",
+            lineHeight: "29px",
+            padding: "0 10px",
+          }}
+        >
+          Back
+        </Typography>
+      </div>
+    </Link>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+  </>
+);
